e65310b0ba01f151cc6ac56a7d0e2790f404db4b
john-2132 <junichi.k0211@gmail.com>
2024-03-15 08:20:58 +0900

    商品一覧の表示機能追加 ・コントローラ、モデルの処理追加 ・ビューの整形 ・seedの作成 ・seedで登録する画像の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Item.create!(
+  name: 'Item',
+  catalog_price: 30.00,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('images/item.jpg')), filename: 'item.jpg')
+)
+
+Item.create!(
+  name: 'Sample',
+  catalog_price: 20.00,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('images/sample.jpg')),
+                                                filename: 'sample.jpg')
+)
+
+Item.create!(
+  name: 'Test',
+  catalog_price: 48.00,
+  sale_price: 24.00,
+  image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('images/test.jpg')), filename: 'test.jpg')
+)
